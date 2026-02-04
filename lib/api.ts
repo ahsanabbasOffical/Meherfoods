6aeafc2f8a6f34e151f2265f298757c90743b3d3
ahsanabbasOffical <ahsanabbasofiical.mail@gmail.com>
2026-02-04 22:47:13 +0500

    change API (2)

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://meherfoods.com/api'
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://sub.meherfoods.com/api'
 
 export interface Product {
   id: number
